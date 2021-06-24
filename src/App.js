266a542fffb49777875991ec783e3d588cb836c9
shane53386 <60024131+shane53386@users.noreply.github.com>
2021-06-24 13:11:10 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React, {PropTypes, Component} from 'react';
-import ReactDOM from 'react-dom';
-import { GoogleMap, Marker } from "react-google-maps"
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import db from "./firebase.js";
 
+const weatherKey = "22f30fcf6dd5b269bf5cbe441f735a39";
 
-const MyMapComponent = (props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-  </GoogleMap>
+function App() {
+	const [place, setPlace] = useState("");
+	const [temp, setTemp] = useState(null);
+	const fetchPlace = async () => {
+		db.collection("Bangkok")
+			.doc("Central World")
+			.get()
+			.then((doc) => {
+				setPlace(doc.data());
+			});
+	};
+	const fetchWeather = async () => {
+		fetch(
+		`https://api.openweathermap.org/data/2.5/onecall?lat=${place.Position.latitude}&lon=${place.Position.longitude}&exclude=hourly,daily,minutely&appid=${weatherKey}`
+	)
+		.then((response) => response.json())
+		.then((data) => setTemp(data.current.temp));
+	}
+	
 
-ReactDOM.render(<MyMapComponent isMarkerShown />, document.getElementById("root"));
+	useEffect(() => {
+		fetchPlace();
+		fetchWeather();
+	}, []);
+
+	return (
+		<div className="container">
+			<div className="pop-up">
+				<h2>{place.Name}</h2>
+				<p>{place.Description}</p>
+				<div> Type : {place.Type}</div>
+				<div> Temp : {temp}</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
