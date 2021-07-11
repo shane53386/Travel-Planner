2bcdd02866488331bcccbae801f7e7d8c41ff728
shane53386 <60024131+shane53386@users.noreply.github.com>
2021-07-11 14:00:59 +0700

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-const firebaseConfig = {
+const firebaseConfig = firebase.initializeApp({
 	apiKey: "AIzaSyAFJbF0GbtVCH70GhIkKAC9qpWoapvclNk",
 	authDomain: "travel-planner-317607.firebaseapp.com",
 	projectId: "travel-planner-317607",
 	storageBucket: "travel-planner-317607.appspot.com",
 	messagingSenderId: "704456727208",
 	appId: "1:704456727208:web:48faa5d958bbd50683409d",
-	measurementId: "G-JQV2031YPF"
-};
+	measurementId: "G-JQV2031YPF",
+});
 
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-const db = firebase.firestore();
+// firebase.analytics();
+export const db = firebaseConfig.firestore();
+export const auth = firebaseConfig.auth();
 
-export default db;
+export default firebaseConfig;
